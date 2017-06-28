7fbf65f5b24c1f1d981964b2ae867e0500b8b109
Ethan Levin <elevin@macalester.edu>
2017-06-27 19:57:00 -0500

    create server and link contact form

--- database.py
+++ database.py
@@ -11,23 +11,22 @@ class Database:
             return self.model.query.get(id)
         return self.model.query.all()
 
-    def create(self, fname, lname, email, interests):
-        person = self.model(fname, lname, email, interests, False)
+    def create(self, name, email, interests):
+        person = self.model(name, email, interests, False)
         self.db.session.add(person)
         self.db.session.commit()
 
-    def update(self, id, fname, lname, email, interests, called):
+    def update(self, id, name, email, interests, contacted):
         person = self.get(id)
-        person.fname = fname
-        person.lname = lname
+        person.name = name
         person.email = email
         person.interests = interests
-        person.called = called
+        person.contacted = contacted
         self.db.session.commit()
 
-    def called(self, id):
+    def contacted(self, id):
         person = self.get(id)
-        person.called = True
+        person.contacted = True
         self.db.session.commit()
 
     def delete(self, id):
@@ -39,16 +38,14 @@ def personFactory(db):
     class Person(db.Model):
         __tablename__ = 'persons'
         id = db.Column('person_id', db.Integer, primary_key=True)
-        fname = db.Column(db.String(20))
-        lname = db.Column(db.String(20))
+        name = db.Column(db.String(100))
         email = db.Column(db.String(100))
         interests = db.Column(db.String)
-        called = db.Column(db.Boolean)
+        contacted = db.Column(db.Boolean)
 
-        def __init__(self, fname, lname, email, interests, called):
-            self.fname = fname
-            self.lname = lname
+        def __init__(self, name, email, interests, contacted):
+            self.name = name
             self.email = email
             self.interests = interests
-            self.called = called
+            self.contacted = contacted
     return Person
---
+++ thesaap.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, url_for, redirect
+from database import Database
+
+app = Flask(__name__)
+app.config.from_pyfile('server.cfg')
+
+# initialize database
+db = Database(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html', db=db)
+
+@app.route('/mission')
+def mission():
+    return render_template('mission.html', db=db)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', db=db)
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'GET':
+        return render_template('contact.html', db=db)
+    else:
+        name = request.form['name']
+        print name
+        email = request.form['email']
+        print email
+        interests = ''
+        print interests
+        if 'discuss' in request.form:
+            discuss = request.form['discuss']
+            interests += discuss
+        if 'leading' in request.form:
+            leading = request.form['leading']
+            interests += leading
+        if 'intouch' in request.form:
+            intouch = request.form['intouch']
+            interests += intouch
+        print interests
+        db.create(name,email,interests)
+        return redirect(url_for('home'))
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html', db=db)
